95cdaefcc13978f479c1632233bbcc8328829539
SCOTT ZUVICH <scott.zuvich@student.csulb.edu>
2022-11-30 20:28:06 -0800

    second commit for tutorial 7

--- index.js
+++ index.js
@@ -1,64 +1,90 @@
-// Add packages
+// Required modules 
+const express = require("express");
+const app = express();
 const dblib = require("./dblib.js");
 
-// Product array
-const products = [
-    {
-        prod_id: 110,
-        prod_name: 'Charger',
-        prod_desc: 'USB',
-        prod_price: 22.50
-    },
-    {
-        prod_id: 200,
-        prod_name: 'Book',
-        prod_desc: 'The JS Way',
-        prod_price: 9.99
-    },
-    {
-        prod_id: 201,
-        prod_name: 'Large Clips',
-        prod_desc: 'Large binder clips',
-        prod_price: 4.25
-    },
-    {
-        prod_id: 202,
-        prod_name: 'Medium Clips',
-        prod_desc: 'Medium binder clips',
-        prod_price: 3.25
-    },
-    {
-        prod_id: 203,
-        prod_name: 'Small Clips',
-        prod_desc: 'Small binder clips',
-        prod_price: 2.25
-    }
-];
-// Declare variables
-var numFailed = 0;
-var numInserted = 0;
-var errorMessage = "";
+const multer = require("multer");
+const upload = multer();
 
-(async () => {
-    console.log("--- STEP 1: Pre-Loop");
-    for (prod of products) {
-        console.log("--- STEP 2: In-Loop Before Insert");
-        const result = await dblib.insertProduct(prod);
-        console.log("--- STEP 3: In-Loop After Insert");
-        console.log("result is: ", result);
-        if (result.trans === "success") {
-            numInserted++;
-        } else {
-            numFailed++;
-            errorMessage += `${result.msg} \r\n`;
-        };
-    };    
-    console.log("--- STEP 4: After-Loop");
-    console.log(`Records processed: ${numInserted + numFailed}`);
-    console.log(`Records successfully inserted: ${numInserted}`);
-    console.log(`Records with insertion errors: ${numFailed}`);
-    if(numFailed > 0) {
-        console.log("Error Details:");
-        console.log(errorMessage);
+// Add middleware to parse default urlencoded form
+app.use(express.urlencoded({ extended: false }));
+
+// Setup EJS
+app.set("view engine", "ejs");
+
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+// Application folders
+app.use(express.static("public"));
+
+// Setup routes
+app.get("/", (req, res) => {
+    //res.send("Root resource - Up and running!")
+    res.render("index");
+});
+app.get("/search", async (req, res) => {
+    // Omitted validation check
+    const totRecs = await dblib.getTotalRecords();
+    //Create an empty product object (To populate form with values)
+    const prod = {
+        prod_id: "",
+        prod_name: "",
+        prod_desc: "",
+        prod_price: ""
     };
-})();
+    res.render("search", {
+        type: "get",
+        totRecs: totRecs.totRecords,
+        prod: prod
+    });
+});
+app.post("/search", async (req, res) => {
+    // Omitted validation check
+    //  Can get totRecs from the page rather than using another DB call.
+    //  Add it as a hidden form value.
+    const totRecs = await dblib.getTotalRecords();
+
+    dblib.findProducts(req.body)
+        .then(result => {
+            res.render("search", {
+                type: "post",
+                totRecs: totRecs.totRecords,
+                result: result,
+                prod: req.body
+            })
+        })
+        .catch(err => {
+            res.render("search", {
+                type: "post",
+                totRecs: totRecs.totRecords,
+                result: `Unexpected Error: ${err.message}`,
+                prod: req.body
+            });
+        });
+});
+
+app.get("/searchajax", async (req, res) => {
+    // Omitted validation check
+    const totRecs = await dblib.getTotalRecords();
+    res.render("searchajax", {
+        totRecs: totRecs.totRecords,
+    });
+});
+app.post("/searchajax", upload.array(), async (req, res) => {
+    dblib.findProducts(req.body)
+        .then(result => res.send(result))
+        .catch(err => res.send({trans: "Error", result: err.message}));
+
+});
+// Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started (http://localhost:3000/) !");
+});
